3234628d31b38ea43e8e52911322f5385962e74e
Haerul Taka <haerultaka255@gmail.com>
2023-05-10 09:49:22 +0700

    optimal caching data

--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -1,7 +1,15 @@
+/* eslint-disable no-undef */
 import { precacheAndRoute } from 'workbox-precaching';
+import { registerRoute } from 'workbox-routing';
+import { StaleWhileRevalidate } from 'workbox-strategies';
 
 // Do precaching
 precacheAndRoute(self.__WB_MANIFEST);
+
+registerRoute(
+  ({ url }) => url.href.startsWith('https://restaurant-api.dicoding.dev'),
+  new StaleWhileRevalidate(),
+);
 
 self.addEventListener('install', () => {
   console.log('Service Worker: Installed');
--- src/scripts/views/app.js
+++ src/scripts/views/app.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import DrawerInitiator from '../utils/drawer-initiator';
 import UrlParser from '../routes/url-parser';
 import routes from '../routes/routes';
@@ -23,11 +24,12 @@ class App {
     const url = UrlParser.parseActiveUrlWithCombiner();
     const page = routes[url];
     this._content.innerHTML = await page.render();
+    await page.afterRender();
 
     const skipLinkElem = document.querySelector('.skip-link');
     skipLinkElem.addEventListener('click', (event) => {
       event.preventDefault();
-      document.querySelector('#mainContent').focus();
+      mainContent.focus();
     });
   }
 }
